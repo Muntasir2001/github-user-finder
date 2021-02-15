edbdb2a571cb077e5325efe8ee854824edac63c5
Muntasir <muntasirenamalhuda@gmail.com>
2021-02-15 09:53:12 +0000

    Fixed UI of the bio section

--- github.js
+++ github.js
@@ -1,11 +1,13 @@
+//?client_id=${this.client_id}&client_secret=${this.client_secret}
+
 class Github {
    constructor() {
-      this.client_id = 'e8b1c629b9601894ddaf';
-      this.client_secret = 'cbebc37be1a24661c5ac84db41e9e53b79525005';
+      this.client_id = '';
+      this.client_secret = '';
    }
 
    async getUser(user) {
-      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+      const profileResponse = await fetch(`https://api.github.com/users/${user}`);
 
       //gives JSON data
       const profileData = await profileResponse.json();
